b04e2b1fb1b3ee6df011bbb3649a6963ec982af5
iLeoDo <iLeoDo@Gmail.com>
2023-09-06 23:05:08 +0100

    [Add] Url query parameters

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,15 +1,15 @@
 const debugMode = false;
-const defaultBooks = ['shijing', 'chuci', 'tangshi', 'songci', 'yuefu', 'gushi', 'cifu'];
+const defaultBooks = ['shijing', 'chuci', 'tangshi', 'songci', 'yuefu', 'gushi', 'cifu', 'custom'];
 const defaultFamilyName = '李';
-const nameAmount = 20;
-const badChars = '低愚贱胸鬼懒禽鸟鸡我邪罪凶丑仇鼠蟋蟀淫秽妹狐鸡鸭蝇悔鱼肉苦犬吠窥血丧饥女搔父母昏狗蟊疾病痛死潦哀痒害蛇牲妇狸鹅穴畜烂兽靡爪氓劫鬣螽毛婚姻匪婆羞辱';
-const maxTry = nameAmount*500;
+const badChars = '愚贱胸鬼懒禽鸟鸡我邪罪凶丑仇鼠蟋蟀淫秽妹狐鸡鸭蝇悔鱼肉苦犬吠窥血丧饥女搔父母昏狗蟊疾病痛死潦哀痒害蛇牲妇狸鹅穴畜烂兽靡爪氓劫鬣螽毛婚姻匪婆羞辱';
+const avoidChars = '奴娑扙低马';
+const maxTry = 10000;
 
 export {
   debugMode,
   defaultBooks,
   defaultFamilyName,
-  nameAmount,
+  avoidChars,
   badChars,
   maxTry,
 };
